c463a92b77687e9e7834ee1823f7197d9e858809
markp <markp@DESKTOP-5592367>
2024-02-21 06:32:54 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GridBoxApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 752);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1440, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1440, 774);
+            this.Controls.Add(this.statusStrip1);
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Non-Profit Demographic Analysis";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace GridBoxApp
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> points = new List<Point>(); // List to store drawn points
+        private int pointSize = 10; // Size of the points
+
+        // Form1 Class Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Properties.Resources.state_zones; // Set the background image (50 United States of America Zones)
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.Resize += (s, e) => this.Invalidate(); // Handler to trigger a repaint, or refresh the image
+            this.MouseMove += Form1_MouseMove; // Subscribe to MouseMove and MouseClick events
+            this.MouseClick += Form1_MouseClick;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Call the base method - ensures other painting is done correctly
+            base.OnPaint(e);
+
+            // Draw points
+            foreach (Point point in points)
+            {
+                DrawPointAtLocation(e.Graphics, point);
+            }
+        }
+
+        // MouseMove event handler
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolStripStatusLabel1.Text = $"Mouse Coordinates: X={e.X}, Y={e.Y}"; // Display mouse coordinates
+        }
+
+        // MouseClick event handler
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            toolStripStatusLabel1.Text = $"Mouse Clicked: X={e.X}, Y={e.Y}"; // Display mouse click coordinates
+
+            if (e.Button == MouseButtons.Left)
+            {
+                DrawPoint(e.Location);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                RemovePoint(e.Location);
+            }
+        }
+
+        // Method to draw a point at specified coordinates
+        private void DrawPoint(Point location)
+        {
+            points.Add(location); // Add the point to the list of points
+            this.Invalidate(); // Invalidate the form to trigger a repaint
+        }
+
+        private void RemovePoint(Point location)
+        {
+            foreach (Point point in points) // Check if there's a point near the clicked location
+            {
+                if (IsPointClicked(point, location))
+                {
+                    points.Remove(point); // Remove the point
+                    this.Invalidate(); // Trigger to repaint [refresh]
+                    break; // Exit loop after removing the point
+                }
+            }
+        }
+
+        // Method to check if a point is clicked
+        private bool IsPointClicked(Point point, Point click)
+        {
+            // Calculate the distance between the point and the click
+            double distance = Math.Sqrt(Math.Pow(point.X - click.X, 2) + Math.Pow(point.Y - click.Y, 2));
+
+            // Return true if the distance is within the point's radius (half of the point size)
+            return distance <= pointSize / 2;
+        }
+
+        // Method to draw a point at a specific location
+        private void DrawPointAtLocation(Graphics g, Point location)
+        {
+            // Calculate the position to draw the point
+            int x = location.X - pointSize / 2;
+            int y = location.Y - pointSize / 2;
+
+            // Draw the point
+            g.FillEllipse(Brushes.Green, x, y, pointSize, pointSize);
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
